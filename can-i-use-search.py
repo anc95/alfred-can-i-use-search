068c6c97a49402e3f041d6192c83da7f7ae8aed2
acnhao <1481988258@qq.com>
2018-06-10 20:47:06 +0800

    first commit

---
+++ can-i-use-search.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+# encoding: utf-8
+
+import sys
+from workflow import Workflow, web
+
+
+ICON_DEFAULT = 'icon.png'
+
+
+# 加载
+def data():
+    url = 'https://raw.githubusercontent.com/Fyrd/caniuse/master/data.json'
+    return web.get(url).json()
+
+def main(wf):
+    
+    # 请求数据
+    use_data = wf.cached_data('data', data, max_age=60 * 60 * 24)['data']
+    # 添加 item 到 workflow 列表
+    list = wf.filter(wf.args[0], use_data, key = lambda o: o)
+
+    if not list:
+        search_url = 'https://caniuse.com/#search=' + wf.args[0]
+        wf.add_item('search on caniuse.com', arg = search_url)
+
+    for key in list:
+        url = 'https://caniuse.com/#feat=spdy' + key
+        val = use_data[key]
+        wf.add_item(
+            title = val['title'],
+            subtitle = val['description'],
+            arg = url,
+            valid = True,
+            icon = ICON_DEFAULT
+        )
+
+    wf.send_feedback()
+    
+
+
+if __name__ == '__main__':
+    
+    wf = Workflow()
+    logger = wf.logger
+    sys.exit(wf.run(main))
